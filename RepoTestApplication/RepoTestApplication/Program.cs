d1913febca45d971f1c6a471b07e17e18dc1d9f0
Emmanuel Omolaja <user@USER-PC>
2017-11-02 22:50:21 -0400

    I just made the changes to this code

--- RepoTestApplication/RepoTestApplication/Program.cs
+++ RepoTestApplication/RepoTestApplication/Program.cs
@@ -8,8 +8,19 @@ namespace RepoTestApplication
 {
     class Program
     {
+        class Student
+        {
+            public int age { get; set; }
+            public string name { get; set; }
+        }
         static void Main(string[] args)
         {
+            Student student = new Student();
+
+            student.age = 45;
+            student.name = "Kehinde Lydia Topeyemi";
+
+            Console.WriteLine("Hello, " + student.name + " I am " + student.age.ToString() + " year old");
             Console.WriteLine("We are here");
         }
     }
